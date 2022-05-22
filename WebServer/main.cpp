a280643d0661369dd8755fe6dfd052b6395ee58e
Yashu-the-great <yashuff1@gmail.com>
2022-05-22 22:31:52 +0530

    Initial Commit

---
+++ WebServer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WebServer
+//
+//  Created by Yashu Maurya on 22/05/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
