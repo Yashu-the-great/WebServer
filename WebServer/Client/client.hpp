da88dbea85a3c2bc828ef5df29b2cb8335496d81
Yashu-the-great <yashuff1@gmail.com>
2022-05-23 22:06:21 +0530

    Client Side example

---
+++ WebServer/Client/client.hpp
@@ -0,0 +1,13 @@
+//
+//  client.hpp
+//  WebServer
+//
+//  Created by Yashu Maurya on 23/05/22.
+//
+
+#ifndef client_hpp
+#define client_hpp
+
+#include <stdio.h>
+#include "../Networking/ClientSocket.hpp"
+#endif /* client_hpp */
---
+++ WebServer/WebServer/webserver.cpp
@@ -0,0 +1,34 @@
+//
+//  webserver.cpp
+//  WebServer
+//
+//  Created by Yashu Maurya on 22/05/22.
+//
+
+#include "webserver.hpp"
+#include "colors.h"
+
+using namespace WS;
+
+int main(void) {
+    Socket socket = Socket(AF_INET, SOCK_STREAM, 0, 6969,  INADDR_ANY, 5);
+    int val_read;
+    while(1) {
+        // simple echo code.
+        green();
+        printf("Listening : \n\n");
+        reset();
+        socket.startAccept(socket.get_sock(), socket.get_address());
+        
+        val_read = read(socket.get_websocket(), socket.buffer, sizeof(socket.buffer));
+        
+        printf("%s\n",socket.buffer);
+        
+        char* hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+        
+        write(socket.get_websocket() , hello , strlen(hello));
+        
+        close(socket.get_websocket());
+    }
+    return 0;
+}
