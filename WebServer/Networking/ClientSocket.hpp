ffc8945bc54b136759b3cb414b3d499f054477fd
Yashu-the-great <yashuff1@gmail.com>
2022-05-23 22:07:26 +0530

    Client Socket Class

--- WebServer/Networking/ClientSocket.hpp
+++ WebServer/Networking/ClientSocket.hpp
@@ -10,11 +10,28 @@
 
 #include <stdio.h>
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <arpa/inet.h>
 
 namespace CWS {
 class ClientSocket {
+private:
+    int client_sock;
+    struct sockaddr_in client_address;
+    int connection;
 public:
-    ClientSocket();
+    char buffer[30000];
+    ClientSocket(int domain, int type, int protocol, int port, char* addr);
+    void checkError(int value);
+    void configureClientAddress(int domain, int port);
+    void checkAddress(int domain, char* addr, struct sockaddr_in address);
+    int clientConnect(int sock, struct sockaddr_in address);
+    int get_client_sock();
+    struct sockaddr_in get_client_address();
+    int get_connection();
 };
 }
 
