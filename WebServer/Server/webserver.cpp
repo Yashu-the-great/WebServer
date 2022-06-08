54be42850c258cc33f6a4532d131b26aac96edbc
Yashu-the-great <yashuff1@gmail.com>
2022-06-08 19:28:55 +0530

    updated according to the lib

--- WebServer/Server/webserver.cpp
+++ WebServer/Server/webserver.cpp
@@ -19,15 +19,10 @@ int main(void) {
         printf("Listening : \n\n");
         reset();
         socket.startAccept(socket.get_sock(), socket.get_address());
-        
-        val_read = read(socket.get_websocket(), socket.buffer, sizeof(socket.buffer));
-        
+        socket.recieve();
         printf("%s\n",socket.buffer);
-        
         char* hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-        
-        write(socket.get_websocket() , hello , strlen(hello));
-        
+        socket.send(hello);
         close(socket.get_websocket());
     }
     return 0;
