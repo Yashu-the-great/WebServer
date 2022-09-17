de2f85965595555140924db3dec25f0f6f7581da
Yashu Maurya <yashumaurya@Yashus-MacBook-Air.local>
2022-09-17 12:01:32 +0530

    update

--- Server/webserver.cpp
+++ Server/webserver.cpp
@@ -22,6 +22,7 @@ int main(void) {
             puts("\n\n-------------------- Request Recieved --------------------\n");
             cout << "Request :: \n" << socket.buffer << endl;
             char* filePath = getFilePath(socket.buffer);
+            
             cout << "Server requested the following file : " << filePath << endl;
             if(strstr(socket.buffer, "/ ") != NULL) {
                 strcat(filePath,"index.html");
